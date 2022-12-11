406418f3c78edb3e21a56ac512a73afcd1959453
Shaun <seansama@pop-os.localdomain>
2022-12-11 14:51:05 +0300

    Callback functions

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-//HTTP requests
-const request = new XMLHttpRequest()
-request.addEventListener(`readystatechange`, () => {
+const getTodos = (callback) => {
 
-    //Status codes
-    if (request.readyState === 4 && request.status === 200){
-        console.log(request.responseText)
+    //HTTP requests
+    const request = new XMLHttpRequest()
+    request.addEventListener(`readystatechange`, () => {
+
+        //Status codes
+        if (request.readyState === 4 && request.status === 200){
+            callback(undefined, request.responseText);
+        }
+        else if (request.readyState === 4){
+           callback(`Data not found`, undefined);
+        }
+    })
+
+
+    request.open(`GET`, `https://jsonplaceholder.typicode.com/todos/`);
+    request.send()
+
+
+}
+//callback functions
+getTodos((err, data) => {
+console.log(`callback fired`)
+    if(err){
+        console.log(err)
     }
-    else if (request.readyState === 4){
-        console.log(`Could not fetch data`)
+    else{
+        console.log(data)
     }
-})
-
-
-request.open(`GET`, `https://jsonplaceholder.typicode.com/todos/`);
-request.send()
-
-//
+});
