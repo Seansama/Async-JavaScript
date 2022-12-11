49aaa5a9d998d92d0199637bc67aded5cb75954e
Shaun <seansama@pop-os.localdomain>
2022-12-11 16:44:18 +0300

    Fetch API and changes to file structure

---
+++ app.js
@@ -0,0 +1,12 @@
+//fetch api
+
+fetch(`https://jsonplaceholder.typicode.com/todos/`).then((response) => {
+    console.log(`resolved`, response)
+return response.json();
+}).then(data => {
+
+console.log(data);
+}).catch((err) => {
+
+console.log(`rejected`, err)
+});
