c3023aa48d165c2303e7230169957a702fb1c423
Shaun <seansama@pop-os.localdomain>
2022-12-11 17:56:38 +0300

    Throwing errors

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const response = await fetch(`https://jsonplaceholder.typicode.com/todos/`)
 return data;
 };
 
-getTodos().then(data => console.log(`resolved:`, data));
+getTodos().then(data => console.log(`resolved:`, data))
+    .catch(err => console.log(`rejected`, err.message));
 
 
 
