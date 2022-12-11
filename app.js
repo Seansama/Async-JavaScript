c83277f696fecc4de98f2d2209f40162e924b488
Shaun <seansama@pop-os.localdomain>
2022-12-11 17:31:04 +0300

    Async await

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+//async and await
+const getTodos = async () => {
+const response = await fetch(`https://jsonplaceholder.typicode.com/todos/`)
+  const data = await response.json();
+return data;
+};
+
+getTodos().then(data => console.log(`resolved:`, data));
+
+
+
+
+
 //fetch api
 
+/*
 fetch(`https://jsonplaceholder.typicode.com/todos/`).then((response) => {
+
     console.log(`resolved`, response)
 return response.json();
 }).then(data => {
@@ -10,3 +25,4 @@ console.log(data);
 
 console.log(`rejected`, err)
 });
+*/
